ed44badd5c49d266a9c3db8b3e4da13d95f3c706
Sarvesh Dandapati <me.sarvd@gmail.com>
2022-02-14 13:32:32 +0530

    added animal trivia questions

---
+++ assets/questions.js
@@ -0,0 +1,47 @@
+const questions = [
+  {
+    questionText: "How many legs does a spider have ?",
+    options: ["1. 6", "2. 4", "3. 5", "4. 8"],
+    answer: "4. 8",
+  },
+  {
+    questionText: "What color are zebras?",
+    options: [
+      "1. White with black stripes",
+      "2. Black with white stripes",
+      "3. Both of the above",
+      "4. None of the above",
+    ],
+    answer: "3. Both of the above",
+  },
+  {
+    questionText: "The slowest Animals In The World is.",
+    options: [
+      "1. Koala",
+      "2. Three-toed sloth",
+      "3. Garden Snail",
+      "4. Giant Tortoise",
+    ],
+    answer: "2. Three-toed sloth",
+  },
+  {
+    questionText: "What is the fastest land animal?",
+    options: [
+      "1. Cheetah",
+      "2. Pronghorn Antelope",
+      "3. Thomson’s Gazelle",
+      "4. Blue Wildebeest",
+    ],
+    answer: "1. Cheetah",
+  },
+  {
+    questionText: "What is the heaviest and tallest Penguin ?",
+    options: [
+      "1. Emperor Penguin",
+      "2. King Penguin",
+      "3. Prince Penguin",
+      "4. Servant Penguin",
+    ],
+    answer: "1. Emperor Penguin",
+  },
+];
--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,112 @@
-const questions = [
-  {
-    questionText: "Commonly used data types DO NOT include:",
-    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    answer: "3. alerts",
-  },
-  {
-    questionText: "Arrays in JavaScript can be used to store ______.",
-    options: [
-      "1. numbers and strings",
-      "2. other arrays",
-      "3. booleans",
-      "4. all of the above",
-    ],
-    answer: "4. all of the above",
-  },
-  {
-    questionText:
-      "String values must be enclosed within _____ when being assigned to variables.",
-    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-    answer: "3. quotes",
-  },
-  {
-    questionText:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: [
-      "1. JavaScript",
-      "2. terminal/bash",
-      "3. for loops",
-      "4. console.log",
-    ],
-    answer: "4. console.log",
-  },
-  {
-    questionText:
-      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
-    options: ["1. break", "2. stop", "3. halt", "4. exit"],
-    answer: "1. break",
-  },
-];
+function log(n) {
+  console.log(n);
+}
+const leaderBoard = document.querySelector("#leaderboard");
+const btn = document.querySelector(".btn");
+const main = document.querySelector("main").classList;
+const qSection = document.querySelector(".q-section").classList;
+const iSection = document.querySelector(".i-section").classList;
+const hSection = document.querySelector(".h-section").classList;
+const quiz = document.querySelector(".quiz");
+const question = document.querySelector(".question");
+const options = document.querySelector(".options");
+const correct = document.querySelector(".correct").classList;
+const incorrect = document.querySelector(".incorrect").classList;
+const userScore = document.querySelector(".userscore");
+const goBack = document.querySelector(".goback");
+const clearScore = document.querySelector(".clear");
+const finalResult = document.querySelector(".final-result");
+const submit = document.querySelector("form");
+let i = 0;
+let countdown = 50;
+let score = 0;
 
-let finalScore = 11;
+function startQuiz() {
+  correct.textContent = "";
+  incorrect.textContent = "";
+  options.innerHTML = "";
+  question.textContent = questions[i].questionText;
+  for (let j = 0; j < 4; j++)
+    options.innerHTML += `<li>${questions[i].options[j]}</li>`;
+}
 
-//countdown timer
-let seconds = 10;
-let time = setInterval(myTimer, 1000);
-function myTimer() {
-    document.querySelector('#time').innerText = seconds;
-    seconds--;
-    if (seconds == -1) {
-        clearInterval(time);
-        allDone(finalScore);
+function endQuiz() {
+  if (countdown < 0 || score === 50 || i === 5) {
+    qSection.add("toggle");
+    iSection.remove("toggle");
+    clearInterval(timer);
+  }
+}
+
+function highScore() {
+  main.toggle("toggle");
+  hSection.toggle("toggle");
+  i = 0;
+  countdown = 50;
+  score = 0;
+  showResult();
+  leaderBoard.addEventListener("click", highScore);
+}
+
+function showResult() {
+  finalResult.innerHTML = "";
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    finalResult.innerHTML += `<li>${key} - ${localStorage.getItem(key)}</li>`;
+  }
+}
+leaderBoard.addEventListener("click", highScore);
+goBack.addEventListener("click", highScore);
+
+btn.addEventListener("click", () => {
+  leaderBoard.removeEventListener("click", highScore);
+  //countdown
+  timer = setInterval(() => {
+    document.querySelector(".countdown").textContent = countdown--;
+    endQuiz();
+  }, 1000);
+  //showing quiz section
+  main.toggle("toggle");
+  qSection.toggle("toggle");
+  startQuiz();
+});
+
+options.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI")
+    if (e.target.textContent === questions[i].answer) {
+      correct.toggle("toggle");
+      setTimeout(() => {
+        i++;
+        score += 10;
+        userScore.textContent = score;
+        endQuiz();
+        startQuiz();
+        correct.toggle("toggle");
+      }, 700);
+    } else {
+      incorrect.toggle("toggle");
+      countdown -= 10;
+      setTimeout(() => {
+        i++;
+        endQuiz();
+        startQuiz();
+        incorrect.toggle("toggle");
+      }, 700);
     }
-}
+});
 
-function allDone(score){
-  document.querySelector('header').innerHTML = '';
-  document.querySelector('body').innerHTML = '';
-  let div = document.createElement('div');
-  div.setAttribute('class', 'slab');
-  let h1 = document.createElement('h1');
-  h1.innerText = 'All Done';
-  let p = document.createElement('p');
-  p.innerHTML =  `Your final score is <span id=final-score'></span>`;
-  let form = document.createElement('form');
-  let label = document.createElement('label');
-  label.setAttribute('for', 'intls');
-  label.innerText = 'Enter Initials: ';
-  let inputForm = document.createElement('input');
-  inputForm.setAttribute('type', 'text');
-  inputForm.setAttribute('id', 'initials');
-  inputForm.setAttribute('name', 'intls');
-  let inputBtn = document.createElement('input');
-  inputBtn.setAttribute('type', 'button');
-  inputBtn.setAttribute('value', 'Submit');
-  document.querySelector('body').append(div);
-  div.append(h1);
-  div.append(p);
-  div.append(form);
-  form.append(label);
-  form.append(inputForm);
-  form.append(inputBtn);
-}
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userName = submit.name.value;
+  localStorage.setItem(userName, score);
+  showResult();
+  iSection.toggle("toggle");
+  hSection.toggle("toggle");
+  correct.innerText = "";
+  incorrect.innerText = "";
+});
 
-
-
+clearScore.addEventListener("click", () => {
+  localStorage.clear();
+  finalResult.innerHTML = "";
+});
